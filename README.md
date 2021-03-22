9568998325eca73e716216b0127396d1e0331161
Patrick Coxall <mr.coxall@mths.ca>
2021-03-22 04:19:22 +0000

    init

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ICS3U-Unit3-01-Python-sales_tax
+# ICS3U, Unit #2-02, Python → Sales Tax
+[![GitHub's Super Linter](https://github.com/Mr-Coxall/ICS3U-Unit3-01-Python-sales_tax/workflows/GitHub's%20Super%20Linter/badge.svg)](https://github.com/Mr-Coxall/ICS3U-Unit3-01-Python-sales_tax/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Mr-Coxall/ICS3U-Unit3-01-Python-sales_tax)](https://repl.it/github/Mr-Coxall/ICS3U-Unit3-01-Python-sales_tax)
---
+++ sales_tax.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2020
+# This program calculates total from subtotal and tax
+
+
+import constants
+
+
+def main():
+    # this function calculates total from subtotal and tax
+
+    # input
+    sub_total = float(input("Enter the subtotal: $"))
+
+    # process
+    tax = sub_total * constants.HST
+    total = sub_total + tax
+
+    # output
+    print("")
+    print(
+        "The HST is ${0:,.2f}, and the total cost is: ${1:,.2f}".format(
+            tax, total
+        )
+    )
+    print("")
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
